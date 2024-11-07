52503b7ecbaf25bb191599bcced4de92e678a900
Harsh Ranjan Pathak <72537619+harshnnn@users.noreply.github.com>
2024-11-07 14:55:26 +0530

    Update Chat.jsx

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import io from "socket.io-client";
 
-const socket = io('http://localhost:5000');
+const socket = io('https://chat-api-git-main-harshnnns-projects.vercel.app/');
 
 const Chat = () => {
     const [message, setMessage] = useState("");
